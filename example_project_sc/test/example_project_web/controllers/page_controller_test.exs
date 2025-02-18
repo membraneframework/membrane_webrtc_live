8ffc2f0b6d2c021366919a11a74f7b3f840deb7c
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2025-02-18 16:22:40 +0100

    Remove old example project

--- example_project_sc/test/example_project_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ExampleProjectWeb.PageControllerTest do
-  use ExampleProjectWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
