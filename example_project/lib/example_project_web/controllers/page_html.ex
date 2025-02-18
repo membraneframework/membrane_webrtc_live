9040ff071e43a8ad776198d214cbac8c7b94a507
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2025-02-18 15:58:00 +0100

    Create new example project

---
+++ example_project/lib/example_project_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule ExampleProjectWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use ExampleProjectWeb, :html
+
+  embed_templates "page_html/*"
+end
